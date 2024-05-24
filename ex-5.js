701ac16088aefc450f07a70cc43d10037ee40b04
Tynnys1120 <53811329+708799@users.noreply.github.com>
2024-05-24 15:06:02 +0700

    js:es6-assignment

--- ex-5.js
+++ ex-5.js
@@ -1,5 +1,16 @@
-function sum() {
+function sum(...input) {
   // Start coding here !
+
+  // Normal for loop
+  //   let result = 0;
+  //   for (let i = 0; i < input.length; i++) {
+  //     result += input[i];
+  //   }
+  //   return result;
+  // }
+
+  //Built in
+  return input.reduce((total, num) => total + num, 0);
 }
 
 let result1 = sum(1, 2, 3, 4, 5, 6, 7, 8);
--- ex-6.js
+++ ex-6.js
@@ -1,4 +1,8 @@
+// Exercise #6: Nullish Coalescing
 let height = undefined;
 let result;
-
+function checkType(height) {
+  return height ?? "Height is not defined";
+}
+result = checkType(height);
 console.log(result); //  Result ควรจะได้ออกมาเป็น “Height is not defined”
